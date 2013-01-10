60c9432ee0b740f0889a3e5f36d2c2cd3c20a3ee
Rob Rodi <robert.rodi@gmail.com>
2013-01-10 10:28:34 -0800

    Init

---
+++ TfsRt/DataModel/Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TfsRt.Data;
+
+namespace TfsRt.DataModel
+{
+    class Model
+    {
+        private ObservableCollection<SampleDataGroup> _types = CreateTfsTypeList();
+
+        internal SummaryModel Summary { get; private set; }
+        internal ObservableCollection<SampleDataGroup> Types { get { return _types; } }
+
+
+        private static ObservableCollection<SampleDataGroup> CreateTfsTypeList()
+        {
+         return    new ObservableCollection<SampleDataGroup>(new[] { new SampleDataGroup(0.ToString(), "WorkItems", "Tfs Work Items", string.Empty, "Work Items" )});
+        }
+
+
+        internal static SampleDataGroup GetGroup(string p)
+        {
+            return new SampleDataGroup(p, "Hello", "World", string.Empty, "stuffs");
+        }
+    }
+}
---
+++ TfsRt/ItemsPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using TfsRt.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TfsRt.DataModel;
+using Windows.UI.ApplicationSettings;
+using TfsRt.Common;
+
+// The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
+
+namespace TfsRt
+{
+    /// <summary>
+    /// A page that displays a collection of item previews.  In the Split Application this page
+    /// is used to display and select one of the available groups.
+    /// </summary>
+    public sealed partial class ItemsPage : TfsRt.Common.LayoutAwarePage
+    {
+        Popup _settingsPopup;
+        private double _settingsWidth;
+        public ItemsPage()
+        {
+            this.InitializeComponent();
+            SettingsPane.GetForCurrentView().CommandsRequested += ItemsPage_CommandsRequested;
+
+        }
+
+        void ItemsPage_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            SettingsCommand cmd = new SettingsCommand("sample", "Sound Options", (x) =>
+            {
+                _settingsPopup = new Popup();
+                _settingsPopup.Closed += OnPopupClosed;
+                Window.Current.Activated += OnWindowActivated;
+                _settingsPopup.IsLightDismissEnabled = true;
+                _settingsPopup.Width = _settingsWidth;
+                _settingsPopup.Height = _windowBounds.Height;
+
+                MainSettings settingsPane = new MainSettings{
+                    Width = _settingsWidth,
+                    Height = Window.Current.Bounds.Height
+                };
+
+                _settingsPopup.Child = settingsPane;
+                _settingsPopup.SetValue(Canvas.LeftProperty, _windowBounds.Width - _settingsWidth);
+                _settingsPopup.SetValue(Canvas.TopProperty, 0);
+                _settingsPopup.IsOpen = true;
+            });
+
+            args.Request.ApplicationCommands.Add(cmd);
+
+            throw new NotImplementedException();
+        }
+
+        private void _settings_Closed(object sender, object e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+            this.DefaultViewModel["Items"] = new Model().Types;// sampleDataGroups;
+        }
+
+        /// <summary>
+        /// Invoked when an item is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // Navigate to the appropriate destination page, configuring the new page
+            // by passing required information as a navigation parameter
+            var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
+    }
+}
