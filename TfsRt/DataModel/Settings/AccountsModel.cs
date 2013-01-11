4a909963f839d053d95b3f3a26ec581b183227f8
Rob Rodi <robert.rodi@gmail.com>
2013-01-11 07:47:35 -0800

    bound to model

--- TfsRt/DataModel/Settings/AccountsModel.cs
+++ TfsRt/DataModel/Settings/AccountsModel.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TfsRt.DataModel.Settings
 {
-    class Account
+    public class Account : INotifyPropertyChanged
     {
+        private Uri _path;
+        private string _userName, _password;
+
+        public Uri Path { 
+            get { return _path; }
+            set { 
+                _path = value;
+                PropertyChangedHelper("Path");
+            } 
+        }
+
+        public string UserName { 
+            get { return _userName;} 
+            set { 
+                _userName = value;
+                PropertyChangedHelper("UserName");
+            }
+        }
+        
+        public string Password { 
+            get { return _password;} 
+            set { 
+                _password = value;
+                PropertyChangedHelper("Password");
+            }
+        }
+
+        private void PropertyChangedHelper(string propertyName) 
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) throw new ArgumentOutOfRangeException("propertyName");
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
-    class AccountsModel
+
+    public class AccountsModel
     {
-        public IObservable<Account> Accounts { get; set; }
+         
+        public ICollection<Account> Accounts { get; set; }
     }
 }
--- TfsRt/Settings/AccountSettings.xaml.cs
+++ TfsRt/Settings/AccountSettings.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using TfsRt.DataModel.Settings;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ApplicationSettings;
@@ -19,23 +21,42 @@ namespace TfsRt.Settings
 {
     public static class SettingsPage
     {
+        internal static void GoBack(UserControl control)
+        {
+            if (control == null) throw new ArgumentNullException("control");
 
+            if (control.Parent.GetType() == typeof(Popup))
+            {
+                ((Popup)control.Parent).IsOpen = false;
+            }
+
+            SettingsPane.Show();
+        }
     }
     public sealed partial class AccountSettings : UserControl
     {
+        public AccountsModel Model { get; set; }
+
         public AccountSettings()
         {
+            Model = new AccountsModel // TODO: Load from storage
+            {
+                Accounts = new []{
+                    new Account { Path = new Uri("http://hello.world"), UserName = "Rob", Password = "RODI" }
+                }
+            };
+            this.DataContext = Model;
             this.InitializeComponent();
         }
 
         private void GoBack(object sender, RoutedEventArgs e)
         {
-            if (this.Parent.GetType() == typeof(Popup))
-            {
-                ((Popup)this.Parent).IsOpen = false;
-            }
+            SettingsPage.GoBack(this);
+        }
 
-            SettingsPane.Show();
+        private void AddAccountClick(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(this.AccountsList.Items.Count);
         }
     }
 }
