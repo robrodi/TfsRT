d2ba001b31e9fedf030f1ac0d0f1adac31b953f7
Rob Rodi <robert.rodi@gmail.com>
2013-01-11 08:38:10 -0800

    Move class libraries to Windows Store Class Libraries

--- TfsRt.Tests/Properties/AssemblyInfo.cs
+++ TfsRt.Tests/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("412cd107-ee1f-4197-97fb-95019c4f0ee7")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
